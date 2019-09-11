4a95e4e0baeac6c12f2198c8af83341c1a55bb79
cyril.bourdieu <cyril.bourdieu@ext.cdiscount.com>
2019-09-11 15:39:17 +0200

    conf OK server OK il manque juste la gestion du DOM pour afficher les resultats => wont do

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: true }),
+    new HtmlWebpackPlugin({ title: 'Pokedex' })
+  ]
+};
